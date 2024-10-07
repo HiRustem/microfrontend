fc5da10cd8166b687446c814474109e6cc055e67
HiRustem <rustemxrid@icloud.com>
2024-10-07 22:58:29 +0300

    micro

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,24 @@
+import NextFederationPlugin from '@module-federation/nextjs-mf';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.output.library = 'microfrontend';
-      config.output.libraryTarget = 'umd';
-      config.output.globalObject = 'window';
-    }
+  webpack(config, { isServer }) {
 
+    if (!isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: 'microfrontend',
+          filename: 'static/chunks/remoteEntry.js',
+          exposes: {
+            './MicroComponent': './src/components/MicrofrontendComponent.tsx'
+          },
+          shared: {
+            react: { singletone: true },
+            'react-dom': { singleton: true },
+          }
+        })
+      )
+    }
     return config;
   }
 };
